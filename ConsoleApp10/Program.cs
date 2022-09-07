c5ed3f08754a46f07e1c0829c652573fff5e543b
Martin Sandung <Martin Sandung@SANDUNG>
2022-09-07 11:56:39 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace dateTime
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatumOmSjuDagar();
+        }
+
+        static public void DatumOmSjuDagar()
+        {
+            var dateTime = DateTime.Now.AddDays(7);
+            Console.WriteLine(dateTime);
+
+        }
+    }
+}
